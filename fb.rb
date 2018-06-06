a2abfe753f478c2f9a865baa30f8c22e36a09594
Sara Beth Ribot <sbribot@gmail.com>
2018-06-06 19:21:51 +0000

    automatically backed up by learn

---
+++ fb.rb
@@ -0,0 +1,10 @@
+fizzbuzz(6)
+# if number is divisible by 3 return fizz
+# if number is divisible by 5 return buzz
+# if number is divisible by 3 and 5 return fizzbuzz 
+# so 6 would return fizz, 10 would return buzz, and 15 would return fizzbuzz
+def fizzbuzz
+  
+  
+  
+end 
--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,18 @@
-def fizzbuzz(number)
-  if number % 5 == 0 && number % 3 == 0
-      "Fizzbuzz"
-    elsif number % 5 == 0 
-      "Buzz"
-  elsif number % 3 == 0  
-    "Fizz"
-else  "nil"
-end
+
+
+
+def fizzbuzz(int)
+  puts "What's your number?"
+  int = gets.chomp
+  if int % 15 == 0
+    puts "FizzBuzz"
+    elsif int % 3==0
+    puts "Fizz"
+    elsif int % 5==0
+    puts "Buzz"
+  else
+    puts "nil"
+  end
 end
 
-fizzbuzz(15)
+fizzbuzz(int)
